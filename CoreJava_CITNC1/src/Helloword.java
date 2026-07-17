7f46a6fa3d905ef512ab2bc585697a58b9e6870e
GitHub User <user@example.com>
2026-07-17 10:34:34 +0530

    first commit

---
+++ CoreJava_CITNC1/src/Helloword.java
@@ -0,0 +1,24 @@
+public class Helloword {
+
+int price =10;
+	
+	void print()
+	{
+		String msg = "hello";
+		System.out.println(msg);
+	}
+    static String message="Hello Word";
+
+
+	
+		
+        
+        public static void main(String[] args) {
+        	Helloword obj =new Helloword();
+        	System.out.println(obj.price);
+        	obj.print();
+        	System.out.println(message);
+        
+	}
+
+}
