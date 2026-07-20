ac2a84685bd862c241d5e046fd6887d00e953ef9
GitHub User <user@example.com>
2026-07-20 10:32:35 +0530

    comit first

---
+++ CoreJava_CITNC1/src/manager/KIA_Rules.java
@@ -0,0 +1,9 @@
+package manager;
+
+public class KIA_Rules {
+
+	
+
+	}
+
+
---
+++ CoreJava_CITNC1/src/sr_developer/KIA_Internal.java
@@ -0,0 +1,12 @@
+package sr_developer;
+
+import jr_developer.KIA_Airbags;
+
+public class KIA_Internal {
+
+	public static void main(String[] args) {
+		KIA_Airbags airbag=new KIA_Airbags();
+		airbag.disp_airbags();
+	}
+
+}
