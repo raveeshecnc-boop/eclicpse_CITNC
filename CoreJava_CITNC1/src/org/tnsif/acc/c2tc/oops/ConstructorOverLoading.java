cc13dddf31ec028654b0be9fa1423c2e830a9026
GitHub User <user@example.com>
2026-07-31 10:34:18 +0530

    commit first

---
+++ CoreJava_CITNC1/src/org/tnsif/acc/c2tc/oops/ConstructorOverLoading.java
@@ -0,0 +1,33 @@
+package org.tnsif.acc.c2tc.oops;
+
+class Student2
+{
+	String name;
+	int age;
+	
+	Student2()
+	{
+		System.out.println("Default constructor");
+	}
+	Student2(String name)
+	{
+		this.name=name;
+		System.out.println("Name "+name);
+	}
+	Student2(String name,int age)
+	{
+		this.name=name;
+		this.age=age;
+		System.out.println("Name "+ name +   "Age "+age);	 
+	}	
+}
+public class ConstructorOverLoading {
+
+	public static void main(String[] args) {
+		Student2  s =new Student2();
+		Student2  s1 =new Student2("Raveesh");
+		Student2  s2 =new Student2("Raveesh",    21);
+
+	}
+
+}
