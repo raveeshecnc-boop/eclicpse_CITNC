2f85f73645b4f15071a5a822c7e06604c65b697f
GitHub User <user@example.com>
2026-08-12 10:20:28 +0530

    commit first

---
+++ CoreJava_CITNC1/src/org/tnsif/acc/c2tc/multipleinterface/MultipleInterfaceDemo.java
@@ -0,0 +1,36 @@
+package org.tnsif.acc.c2tc.multipleinterface;
+
+class SmartPhone implements Camera,MusicPlayer
+{
+
+	@Override
+	public void playMusic() {
+		System.out.println("Playing music on the smartphone");
+		
+	}
+
+	@Override
+	public void takePhoto() {
+		System.out.println("Taking photo using smartphone camera");
+		
+	}
+
+	@Override
+	public void playMUsic() {
+		System.out.println("Playing music on the smartphone");
+		
+	}
+	
+}
+
+
+public class MultipleInterfaceDemo {
+
+	public static void main(String[] args) {
+		SmartPhone smartphone=new SmartPhone();
+		smartphone.playMusic();
+		smartphone.takePhoto();
+
+	}
+
+}
